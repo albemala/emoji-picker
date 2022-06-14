78ea08dec149c2535f0ffbc5abc8f80f07766312
Unknown <albemala@gmail.com>
2022-06-14 11:45:09 +0200

    about dialog: added link to repo and reworked layout

--- lib/blocs/urls-bloc.dart
+++ lib/blocs/urls-bloc.dart
@@ -2,6 +2,7 @@ import 'package:url_launcher/url_launcher.dart';
 
 const _supportEmail = 'mailto:albemala@gmail.com';
 const _twitter = 'https://twitter.com/albemala';
+const _repository = 'https://github.com/albemala/emoji-picker';
 const _exaboxWebsite = 'https://exabox.app';
 
 class UrlsBloc {
@@ -15,6 +16,10 @@ class UrlsBloc {
 
   Future<void> openExaboxWebsite() async {
     await _open(_exaboxWebsite);
+  }
+
+  Future<void> openRepository() async {
+    await _open(_repository);
   }
 
   Future<void> _open(String url) async {
--- lib/views/about-view.dart
+++ lib/views/about-view.dart
@@ -1,4 +1,5 @@
 import 'package:app/providers.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -22,15 +23,27 @@ class AboutView extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return ConstrainedBox(
-      constraints: const BoxConstraints(maxWidth: 480),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: const [
-          _AboutContentView(),
-          _AdView(),
-        ],
+    return SingleChildScrollView(
+      child: ConstrainedBox(
+        constraints: const BoxConstraints(maxWidth: 480),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24, vertical: 32),
+              child: _AboutContentView(),
+            ),
+            Material(
+              surfaceTintColor: Theme.of(context).colorScheme.primary,
+              elevation: Theme.of(context).brightness == Brightness.light ? 1 : 12,
+              child: const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 24, vertical: 32),
+                child: _AdView(),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -41,23 +54,15 @@ class _AboutContentView extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 32),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Wrap(
-            spacing: 32,
-            runSpacing: 24,
-            children: const [
-              _AppInfoView(),
-              _SupportView(),
-              _SocialView(),
-            ],
-          ),
-        ],
-      ),
+    return Wrap(
+      spacing: 24,
+      runSpacing: 16,
+      children: const [
+        _AppInfoView(),
+        _SupportView(),
+        _SocialView(),
+        _RepositoryView(),
+      ],
     );
   }
 }
@@ -78,6 +83,7 @@ class _AppInfoView extends HookConsumerWidget {
         ),
         const SizedBox(width: 16),
         Column(
+          mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
@@ -115,6 +121,7 @@ class _SupportView extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Column(
+      mainAxisSize: MainAxisSize.min,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         const Text('Help & Support'),
@@ -123,7 +130,10 @@ class _SupportView extends HookConsumerWidget {
           onPressed: () {
             ref.read(urlsProvider).sendFeedback();
           },
-          child: const Text('Send Feedback'),
+          child: const Text(
+            'Send Feedback',
+            maxLines: 1,
+          ),
         ),
       ],
     );
@@ -136,6 +146,7 @@ class _SocialView extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Column(
+      mainAxisSize: MainAxisSize.min,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         const Text('Follow me!'),
@@ -151,60 +162,73 @@ class _SocialView extends HookConsumerWidget {
   }
 }
 
+class _RepositoryView extends HookConsumerWidget {
+  const _RepositoryView();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return RichText(
+      text: TextSpan(
+        text: 'Source code available on ',
+        style: Theme.of(context).textTheme.bodyText2,
+        children: [
+          TextSpan(
+            text: 'GitHub',
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.primary,
+            ),
+            recognizer: TapGestureRecognizer()
+              ..onTap = () {
+                ref.read(urlsProvider).openRepository();
+              },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
 class _AdView extends HookConsumerWidget {
   const _AdView();
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Material(
-      surfaceTintColor: Theme.of(context).colorScheme.primary,
-      elevation: Theme.of(context).brightness == Brightness.light ? 1 : 12,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 32),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Image.asset(
-              'assets/images/exabox-icon.png',
-              width: 48,
-              height: 48,
-            ),
-            const SizedBox(width: 16),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Are you a software developer?'.toUpperCase(),
-                    style: Theme.of(context).textTheme.caption,
-                  ),
-                  Text(
-                    'Exabox: Essential tools for developers',
-                    style: Theme.of(context).textTheme.headline6,
-                  ),
-                  const SizedBox(height: 8),
-                  Text(
-                    '''
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Image.asset(
+          'assets/images/exabox-icon.png',
+          width: 48,
+          height: 48,
+        ),
+        const SizedBox(height: 16),
+        Text(
+          'Are you a software developer?'.toUpperCase(),
+          style: Theme.of(context).textTheme.caption,
+        ),
+        Text(
+          'Exabox: Essential tools for developers',
+          style: Theme.of(context).textTheme.headline6,
+        ),
+        const SizedBox(height: 8),
+        Text(
+          '''
 30+ tools: convert/format JSON/YAML, encode/decode Base64, generate fake data, parse JWTs, transform text using multiple rules, and much more. 
 All in one single app. Work offline. Privacy-friendly.''',
-                    style: Theme.of(context).textTheme.bodyText2,
-                  ),
-                  const SizedBox(height: 4),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: TextButton(
-                      onPressed: () {
-                        ref.read(urlsProvider).openExaboxWebsite();
-                      },
-                      child: const Text('Learn more'),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
+          style: Theme.of(context).textTheme.bodyText2,
         ),
-      ),
+        const SizedBox(height: 4),
+        Align(
+          alignment: Alignment.centerRight,
+          child: TextButton(
+            onPressed: () {
+              ref.read(urlsProvider).openExaboxWebsite();
+            },
+            child: const Text('Learn more'),
+          ),
+        ),
+      ],
     );
   }
 }
