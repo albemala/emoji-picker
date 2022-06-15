ec183e016cc7c079cd51dde47ca2c0df8b4a5ba8
Unknown <albemala@gmail.com>
2022-06-15 15:17:58 +0200

    change system bar color based on selected theme

--- lib/views/app-view.dart
+++ lib/views/app-view.dart
@@ -2,6 +2,7 @@ import 'package:app/assets/app-theme.dart';
 import 'package:app/providers.dart';
 import 'package:app/views/main-view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class AppView extends HookConsumerWidget {
@@ -12,6 +13,13 @@ class AppView extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final theme = ref.watch(themeProvider);
+
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        statusBarBrightness: theme == ThemeMode.dark ? Brightness.dark : Brightness.light,
+      ),
+    );
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Ejimo',
