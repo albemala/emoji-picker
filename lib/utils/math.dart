08065d2dd345e96606bb8173365739edb1cea29a
albemala <albemala@gmail.com>
2023-04-11 14:13:46 +0200

    showcasing other projects in main screen

--- lib/utils/math.dart
+++ lib/utils/math.dart
@@ -2,10 +2,16 @@ import 'dart:math';
 
 /// Returns a random integer between [min] and [max] (inclusive).
 int randomInt(int min, int max) {
+  if (min > max) {
+    throw ArgumentError('min must be less than or equal to max');
+  }
   return min + Random().nextInt(max - min + 1);
 }
 
 /// Checks if [number] is between [min] and [max] (inclusive).
 bool isBetween(int number, int min, int max) {
+  if (min > max) {
+    throw ArgumentError('min must be less than or equal to max');
+  }
   return number >= min && number <= max;
 }
--- lib/views/glyph-list.dart
+++ lib/views/glyph-list.dart
@@ -1,4 +1,6 @@
 import 'package:app/providers.dart';
+import 'package:app/utils/math.dart';
+import 'package:app/views/ads.dart';
 import 'package:app/views/glyph-group-list.dart';
 import 'package:app/views/glyph-group-title.dart';
 import 'package:flutter/material.dart';
@@ -22,10 +24,72 @@ class GlyphListView extends HookConsumerWidget {
             children: [
               GlyphGroupTitleView(title: entry.key),
               GlyphGroupListView(glyphs: entry.value),
+              const _AdView(),
             ],
           );
         }).toList(),
       ),
+    );
+  }
+}
+
+class _AdView extends StatelessWidget {
+  const _AdView();
+
+  @override
+  Widget build(BuildContext context) {
+    // randomize which ad to show
+    final number = randomInt(1, 120);
+    if (isBetween(number, 1, 3)) {
+      return const _AdContainerView(
+        child: ExaboxAdView(),
+      );
+    }
+    if (isBetween(number, 11, 13)) {
+      return const _AdContainerView(
+        child: HexeeProAdView(),
+      );
+    }
+    if (isBetween(number, 21, 23)) {
+      return const _AdContainerView(
+        child: WMapAdView(),
+      );
+    }
+    if (isBetween(number, 31, 33)) {
+      return const _AdContainerView(
+        child: IroIronAdView(),
+      );
+    }
+    return const SizedBox();
+  }
+}
+
+class _AdContainerView extends StatelessWidget {
+  final Widget child;
+
+  const _AdContainerView({
+    required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        const SizedBox(
+          height: 2,
+          child: Material(),
+        ),
+        Center(
+          child: ConstrainedBox(
+            constraints: const BoxConstraints(maxWidth: 480),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 32),
+              child: child,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
