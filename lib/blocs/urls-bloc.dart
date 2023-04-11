5bc072444077cb627bd5c510f07610299be07b74
albemala <albemala@gmail.com>
2023-04-11 13:48:58 +0200

    using send_support_email package

--- lib/blocs/urls-bloc.dart
+++ lib/blocs/urls-bloc.dart
@@ -1,40 +1,42 @@
+import 'package:send_support_email/send_support_email.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-const _supportEmail = 'mailto:albemala@gmail.com';
-const _twitter = 'https://twitter.com/albemala';
-const _repository = 'https://github.com/albemala/emoji-picker';
-const _exaboxWebsite = 'https://exabox.app';
-const _hexeeWebsite = 'https://hexee.app/';
-const _wmapWebsite = 'https://wmap.albemala.me/';
-const _iroIroWebsite = 'https://iro-iro.albemala.me/';
+const _supportEmailUrl = 'mailto:albemala@gmail.com';
+const _twitterUrl = 'https://twitter.com/albemala';
+const _repositoryUrl = 'https://github.com/albemala/emoji-picker';
+const _exaboxWebsiteUrl = 'https://exabox.app';
+const _hexeeWebsiteUrl = 'https://hexee.app/';
+const _wmapWebsiteUrl = 'https://wmap.albemala.me/';
+const _iroIroWebsiteUrl = 'https://iro-iro.albemala.me/';
 
 class UrlsBloc {
   Future<void> sendFeedback() async {
-    await _open(_supportEmail);
+    final supportEmail = await generateSupportEmail(_supportEmailUrl);
+    await _open(supportEmail);
   }
 
   Future<void> openTwitter() async {
-    await _open(_twitter);
+    await _open(_twitterUrl);
   }
 
   Future<void> openExaboxWebsite() async {
-    await _open(_exaboxWebsite);
+    await _open(_exaboxWebsiteUrl);
   }
 
   Future<void> openHexeeWebsite() async {
-    await _open(_hexeeWebsite);
+    await _open(_hexeeWebsiteUrl);
   }
 
   Future<void> openWMapWebsite() async {
-    await _open(_wmapWebsite);
+    await _open(_wmapWebsiteUrl);
   }
 
   Future<void> openIroIroWebsite() async {
-    await _open(_iroIroWebsite);
+    await _open(_iroIroWebsiteUrl);
   }
 
   Future<void> openRepository() async {
-    await _open(_repository);
+    await _open(_repositoryUrl);
   }
 
   Future<void> _open(String url) async {
