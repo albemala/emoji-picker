85787c00a474c930a7ee71a0340ff2d10d8944dd
albemala <albemala@gmail.com>
2023-10-06 12:42:07 +0200

    updated app theme

--- lib/defines/app-theme.dart
+++ lib/defines/app-theme.dart
@@ -1,22 +1,39 @@
 import 'package:flex_color_scheme/flex_color_scheme.dart';
+import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 final lightTheme = FlexThemeData.light(
   scheme: FlexScheme.material,
+  surfaceMode: FlexSurfaceMode.levelSurfacesLowScaffold,
+  blendLevel: 7,
   subThemesData: const FlexSubThemesData(
     defaultRadius: 32,
+    useM2StyleDividerInM3: true,
   ),
-  blendLevel: 8,
+  keyColors: const FlexKeyColors(
+    useSecondary: true,
+    useTertiary: true,
+  ),
+  tones: FlexTones.vivid(Brightness.light),
   useMaterial3: true,
+  visualDensity: VisualDensity.compact,
   fontFamily: GoogleFonts.titilliumWeb().fontFamily,
 );
 
 final darkTheme = FlexThemeData.dark(
   scheme: FlexScheme.material,
+  surfaceMode: FlexSurfaceMode.levelSurfacesLowScaffold,
+  blendLevel: 13,
   subThemesData: const FlexSubThemesData(
+    useM2StyleDividerInM3: true,
     defaultRadius: 32,
   ),
-  blendLevel: 24,
+  keyColors: const FlexKeyColors(
+    useSecondary: true,
+    useTertiary: true,
+  ),
+  tones: FlexTones.vivid(Brightness.dark),
   useMaterial3: true,
+  visualDensity: VisualDensity.compact,
   fontFamily: GoogleFonts.titilliumWeb().fontFamily,
 );
