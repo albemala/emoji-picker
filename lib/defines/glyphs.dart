c2005b421d5a89db0a6e83a1c53f3a354c7d33f9
albemala <albemala@gmail.com>
2023-10-17 10:36:20 +0200

    removed "noto color emoji", using fallback fonts instead

--- lib/defines/glyphs.dart
+++ lib/defines/glyphs.dart
@@ -1,11 +1,16 @@
 import 'package:app/models/glyph.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 TextStyle getTextStyleForGlyph(Glyph glyph) {
   switch (glyph.type) {
     case GlyphType.emoji:
-      return GoogleFonts.notoColorEmoji();
+      return const TextStyle(
+        fontFamilyFallback: [
+          'Apple Color Emoji', // iOS and macOS
+          'Segoe UI Emoji', // Windows
+          'Android Emoji', // Android
+        ],
+      );
     case GlyphType.symbol:
       return const TextStyle();
     case GlyphType.kaomoji:
